66fb0ae85b7d4b545a6faeeae0aa9cc6e5f0f80e
Nils Karlbrink Malmquist <71669738+NilsKarlbrinkMalmquist@users.noreply.github.com>
2021-02-24 18:29:36 +0100

    C4 finished

--- C_Exercises/Eclipse/C4/src/SimpleGUI.java
+++ C_Exercises/Eclipse/C4/src/SimpleGUI.java
@@ -20,9 +20,32 @@ public class SimpleGUI {
 
     public void initializeGUI() {
         //TODO C4.E1: Write your GUI code here //
-        
-        // ActionListener
+    	JFrame frame = new JFrame(name);
+    	JPanel pane = new JPanel();
+    	JButton button = new JButton("Press");
+    	JButton quitbutton = new JButton("Quit");
+    	JLabel label = new JLabel(" ");
+    	
+    	pane.setLayout(new BorderLayout());
+    	pane.add(button, BorderLayout.SOUTH);
+    	pane.add(quitbutton, BorderLayout.NORTH);
+    	pane.add(label, BorderLayout.CENTER);
+    	    	
+    	frame.getContentPane().add(pane, BorderLayout.CENTER);
+    	frame.pack();
+    	frame.setVisible(true);
+
         //TODO C4.E2: Write your anonymous listener here //
+    	button.addActionListener(event -> label.setText("Hello World!"));
+    	
+    	quitbutton.addActionListener(new ActionListener() {
+    		public  void actionPerformed(ActionEvent e) {
+    			 System.out.println(Thread.currentThread().getPriority());
+    			 System.exit(0);
+    	    }
+    	});
+    	
+    	
         //TODO C4.E3: Write code to find the event-dispatching thread's priority, here //
     }
 
